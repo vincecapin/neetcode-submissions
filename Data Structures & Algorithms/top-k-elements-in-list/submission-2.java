6dd8aa188d21db12a3923032094bad222806240f
vincecapin <146299131+vincecapin@users.noreply.github.com>
2026-08-29 17:46:59 +0800

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num : nums){
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<int[]> freqQueue = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()){
+            freqQueue.offer(new int[]{entry.getValue(), entry.getKey()});
+            if (freqQueue.size() > k){
+                freqQueue.poll();
+            }
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+        while (!freqQueue.isEmpty()){   
+            result[index] = freqQueue.poll()[1];
+            index++;
+        }
+
+        return result;
+    }
+}
