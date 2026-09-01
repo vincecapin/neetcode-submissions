9b16a5fb7ecaa910f1181d9d296a3dc734c59095
vincecapin <146299131+vincecapin@users.noreply.github.com>
2026-09-01 14:14:49 +0800

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+
+        List<Integer> sizes = new ArrayList<>();
+        StringBuilder result = new StringBuilder();
+
+        for (String str : strs) {
+            sizes.add(str.length());
+        }
+
+        for (int size : sizes) {
+            result.append(size).append(',');
+        }
+
+        result.append('#');
+
+        for (String str : strs) {
+            result.append(str);
+        }
+
+        return result.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<Integer> sizes = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+
+        int index = 0;
+        while (str.charAt(index) != '#') {
+            StringBuilder currentChar = new StringBuilder();
+            while (str.charAt(index) != ',') {
+                currentChar.append(str.charAt(index));
+                index++;
+            }
+            sizes.add(Integer.parseInt(currentChar.toString()));
+            index++;
+        }
+
+        index++;
+        for (int size : sizes) {
+            result.add(str.substring(index, index + size));
+            index += size;
+        }
+
+        return result;
+
+    }
+}
